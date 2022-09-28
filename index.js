04144326c4a6163e1c442be05eb689dcd1b13783
hungnm <hungnm@umee.com.vn>
2022-09-28 17:35:41 +0700

    add keep alive

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ require('./bootstrap');
 require('./instant');
 require('./logic');
 require('./router');
+
+// keep heroku
+var http = require("http");
+try {
+  setInterval(function () {
+    http.get("https://chat-with-idol-client.herokuapp.com/");
+    http.get("https://chat-with-idol-server.herokuapp.com/");
+    
+    console.log("====================================");
+    console.log("keep alive");
+    console.log("====================================");
+  }, 300000);
+} catch (error) {
+  console.log("====================================");
+  console.log("keep alive error", JSON.stringify(error));
+  console.log("====================================");
+}
