85c304dcf30e51a0c8c9fb8ce24f3d10731f3e58
hungnm <hungnm@umee.com.vn>
2022-09-29 09:40:18 +0700

    add leave room

--- logic.js
+++ logic.js
@@ -1,45 +1,52 @@
 const { io } = require("./instant");
 
 io.on("connection", function (socket) {
+  const { id, name } = socket.handshake.query;
 
-  const {
-    id, name 
-  }= socket.handshake.query;
-
-    console.log('====================================');
-    console.log(`${socket.id} - ${name} connected`);
-    console.log('====================================');
+  console.log("====================================");
+  console.log(`${socket.id} - ${name} connected`);
+  console.log("====================================");
 
   socket.on("join-room", ({ roomId }) => {
     socket.join(roomId);
     io.to(roomId).emit("new_user_join", { name, id });
-    console.log('====================================');
+    console.log("====================================");
     console.log(`${id}-${name} joined to the room ${roomId}`);
-    console.log('====================================');
+    console.log("====================================");
   });
 
-
   socket.on("new_message", function ({ text, roomId }) {
-    console.log('====================================');
-    console.log('new msg', text, roomId );
-    console.log('====================================');
-    io.to(roomId).emit('new_message', { text, name, id })
+    console.log("====================================");
+    console.log("new msg", text, roomId);
+    console.log("====================================");
+    io.to(roomId).emit("new_message", { text, name, id });
   });
 
   socket.on("typing", function ({ msg, roomId }) {
-    socket.to(roomId).emit('typing', { msg, name, id })
+    io.to(roomId).emit("typing", { msg, name, id });
+  });
+
+  socket.on("disconnecting", () => {
+    console.log('====================================');
+    socket.rooms.forEach(roomId => {
+      io.to(roomId).emit('new_message', { text:`${name} left the conversation`, name, id } )
+      console.log('====================================');
+      console.log(roomId, 'disconnected');
+      console.log('====================================');
+    });
+    console.log('====================================');
   });
 
   socket.on("disconnect", function () {
-    console.log('====================================');
-    console.log(`client ${socket.id} disconnected`);
-    console.log('====================================');
+    console.log("====================================");
+    console.log(`client ${socket.id} disconnected`, socket.rooms);
+    console.log("====================================");
   });
 
-  socket.on('leave_room', ({ roomId })=>{
-    console.log('====================================');
+  socket.on("leave_room", ({ roomId }) => {
+    console.log("====================================");
     console.log(id, ` leave the room `, roomId);
-    console.log('====================================');
-    socket.leave(roomId)
+    console.log("====================================");
+    socket.leave(roomId);
   });
 });
